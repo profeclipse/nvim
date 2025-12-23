00e49b05ce0ad632961c73a3c249ae9111dfb58b
profeclipse <davehunt@me.com>
2025-12-23 17:11:52 -0500

    Added keymap for fuzzy help

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -11,7 +11,7 @@ return {
     vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua grep<cr>", { desc = "Fuzzy grep files" })
     vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua grep_curbuf<cr>", { desc = "Fuzzy grep current buffer" })
     vim.keymap.set("n", "<leader>fw", "<cmd>FzfLua grep_cword<cr>", { desc = "Fuzzy grep word under cursor" })
-    --vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+    vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Fuzzy search help tags" })
     --vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols)
     --vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols)
   end,
