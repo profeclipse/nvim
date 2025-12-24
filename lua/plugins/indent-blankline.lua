5128b82bf3299f641d96660882c865b69fe41f57
profeclipse <davehunt@me.com>
2025-12-24 13:16:37 -0500

    Playing with indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,8 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
+  dependencies = { "HiPhish/rainbow-delimiters.nvim" },
+  event        = { "BufReadPre", "BufNewFile" },
+  config       = function()
     local highlight = {
       "RainbowRed",
       "RainbowYellow",
@@ -11,9 +12,9 @@ return {
       "RainbowViolet",
       "RainbowCyan",
     }
-    local ibl = require("ibl")
-    local hooks = require("ibl.hooks")
-
+    local hooks = require "ibl.hooks"
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
       vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -24,12 +25,9 @@ return {
       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
 
-    local opts = {
-      indent = {
-        char = "▏",
-        highlight = highlight,
-      },
-    }
-    ibl.setup(opts)
+    vim.g.rainbow_delimiters = { highlight = highlight }
+    require("ibl").setup { scope = { highlight = highlight } }
+
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
 }
