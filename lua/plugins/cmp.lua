022e89c96cace16b9a17c733b5f58015c5001cb3
profeclipse <davehunt@me.com>
2025-12-24 13:54:49 -0500

    Fixed auto-completion CR behavior

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,7 +28,18 @@ return {
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping({
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              fallback()
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = {
