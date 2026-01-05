01b9696044f885c52c76a3cc90a6e1bb4f72123c
profeclipse <davehunt@me.com>
2026-01-05 17:34:01 -0500

    reworked to use blink and cleaned up

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,76 @@
+return {
+  "Saghen/blink.cmp",
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "fang2hou/blink-copilot",
+    "Kaiser-Yang/blink-cmp-dictionary",
+  },
+  version = "1.*",
+  opts = {
+    snippets = { preset = "luasnip" },
+    keymap = { preset = "super-tab" },
+    completion = {
+      ghost_text = { enabled = true },
+      list = { selection = { auto_insert = false } },
+      documentation = { auto_show = true, window = { border = "rounded" } },
+      menu = {
+        draw = {
+          padding = 0,
+          columns = {
+            { "kind_icon", gap = 1 },
+            { gap = 1,     "label" },
+            { "kind",      gap = 2 },
+          },
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                return " " .. ctx.kind_icon .. " "
+              end,
+              highlight = function(ctx)
+                return "BlinkCmpKindIcon" .. ctx.kind
+              end,
+            },
+            kind = {
+              text = function(ctx)
+                return " " .. ctx.kind .. " "
+              end,
+            },
+          },
+        },
+      },
+    },
+    sources = {
+      default = {
+        "lsp",
+        "path",
+        "snippets",
+        "buffer",
+        "dictionary",
+        "copilot",
+      },
+      providers = {
+        dictionary = {
+          module = "blink-cmp-dictionary",
+          min_keyword_length = 3,
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+        lsp = {
+          score_offset = 90,
+        },
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          async = true,
+          score_offset = -1,
+        },
+      },
+    },
+    fuzzy = { implementation = "lua" },
+  },
+  opts_extend = { "sources.default" },
+}
