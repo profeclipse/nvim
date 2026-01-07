735c3064fcf387f95b532dc4bc2295c3fcce2a21
profeclipse <davehunt@me.com>
2026-01-06 20:54:44 -0500

    Removed redundant keymaps

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,14 +8,6 @@ return {
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
         callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-          map("<leader>cd", vim.lsp.buf.hover, "[C]ode [D]ocumentation", { "n", "x" })
-
           local function client_supports_method(client, method, bufnr)
             if vim.fn.has "nvim-0.11" == 1 then
               return client:supports_method(method, bufnr)
